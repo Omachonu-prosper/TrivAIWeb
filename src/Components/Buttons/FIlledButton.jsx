24847f13ca0e10ac0ec01870500b56d34a699540
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-07-24 00:15:36 +0100

    Created reusable btn components

---
+++ src/Components/Buttons/FIlledButton.jsx
@@ -0,0 +1,5 @@
+const FilledButton = ({ text }) => {
+  return <button className="font-nunito font-[700] bg-gradient-to-r from-[#9068C1] to-[#3892DB] border border-transparent hover:border-purple-400 hover:to-[#9068C1] hover:from-[#3892DB] px-[30px] py-[10px] rounded-[6px] cursor-pointer">{text}</button>;
+};
+
+export default FilledButton;
