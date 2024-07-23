6855ddf3dce844aa06b8ecfcba0a291fa319fe94
james <abrahamjames543@gmail.com>
2024-07-22 19:17:30 -0700

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+import { Layout, Hero } from "@/Components";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Hero />
+    </Layout>
+  );
+}
