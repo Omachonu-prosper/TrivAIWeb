9162da03605e6d5f7dd670d1cddda1cc8a03f279
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-07-24 00:18:36 +0100

    Implemented the navigation bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 // images
-import logo from '../../public/Logo.png'
+import logo from "../../public/Logo.png";
+import FilledButton from "./Buttons/FIlledButton";
+import TextButton from "./Buttons/TextButton";
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className="container w-[830px] h-[90px] z-50 mx-auto bg-[#1e1e1e] bg-opacity-30 rounded-[28px] px-[42px] py-[15px] mt-[30px] text-[#fefefe] flex items-center">
       <Image src={logo} />
-      <div>
-        
-      </div>
-    </div>
-  )
-}
+      <ul className="ml-[70px] flex gap-[28px] items-center">
+        <li>
+          <TextButton text={"How to play"}/>
+        </li>
+        <li>
+          <TextButton text={"About Us"}/>
+        </li>
+        <li>
+          <FilledButton text="Join a game" />
+        </li>
+        <li>
+          <FilledButton text="Create a game" />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 // cetaphil - 17k
 
-
-export default Navbar
+export default Navbar;
