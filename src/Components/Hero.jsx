881a70e0ecbdbf2080a63884214b9a2a73600b8d
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-07-26 23:25:53 +0100

    Mobile responsiveness

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,18 +9,18 @@ const Hero = () => {
     <div className="relative flex flex-col">
       <Image
         src={heroImage}
-        className="w-[75%] z-0 inset-0 absolute left-[50%] -translate-x-[50%]"
+        className="lg:w-[75%] z-0 inset-0 absolute left-[50%] translate-y-[120%] -translate-x-[50%]"
       />
 
-      <div className="z-30 flex flex-col gap-[36px] lg:w-[830px] mx-auto mt-[70px]">
-        <div className="flex flex-col items-center font-nunito font-[900] text-[50px] text-[#fefefe]">
+      <div className="z-30 flex flex-col lg:gap-[36px] gap-[20px] lg:w-[830px] mx-auto lg:mt-[70px] mt-16">
+        <div className="flex flex-col items-center font-nunito font-[900] text-4xl lg:text-[50px] text-[#fefefe]">
           <h2 className="text-center">
             Unleash <span className="gradient-text">Your Trivia Genius </span>{" "}
             with
           </h2>
           <h2 className="text-center">AI-Powered Challenges!</h2>
         </div>
-        <div className="text-[#fefefe]">
+        <div className="text-[#fefefe] font-nunito">
           <p className="font-[400] text-[20px] text-center p-2">
             Join the ultimate trivia showdown and compete against players
             worldwide. Experience smart, fun, and dynamic quizzes powered by
@@ -33,7 +33,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="bg-[#d9d9d9] mt-[40px] px-32 py-10 z-10 bg-opacity-20 blur-bottom relative overflow-hidden">
+      <div className="bg-[#d9d9d9] mt-[40px] px-2 py-5 lg:px-32 lg:py-10 z-10 bg-opacity-20 blur-bottom relative overflow-hidden">
         <Image src={browserDark} className="opacity-100z"/>
       </div>
     </div>
