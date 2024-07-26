41f1e8785626e7256f93b3d00d5013b91866a831
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-07-26 23:06:16 +0100

    proper spacing

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -6,7 +6,7 @@ import FilledButton from './Buttons/FIlledButton'
 
 const AboutUs = () => {
   return (
-    <div className='h-max mt-10 lg:mt-16 w-[88%] container mx-auto text-white grid lg:grid-cols-3 items-start justify-center lg:gap-14 gap-8 font-nunito'>
+    <div className='h-max mt-10 lg:mt-16 mb-[100px] w-[88%] container mx-auto text-white grid lg:grid-cols-3 items-start justify-center lg:gap-14 gap-8 font-nunito'>
       <Image src={img}  width={350} className='object-cover bg-custom rounded-tl-[200px] rounded-md lg:block hidden' />
       <div className='flex flex-col items-start justify-around col-span-2 gap-[10px]'>
         <h1 className='text-center self-center lg:self-start font-[800] text-[36px] '><span className="gradient-text">About</span> Us</h1>
